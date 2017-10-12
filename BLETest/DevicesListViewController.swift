ee2519d79e2de5f4ef8f83d67799389e596a73ed
alexclp <alexandru.clapa@gmail.com>
2017-10-12 16:28:25 +0100

    Added comments

--- BLETest/DevicesListViewController.swift
+++ BLETest/DevicesListViewController.swift
@@ -18,13 +18,15 @@ class DevicesListViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
 		
+		// Initialising the central manager and setting the delegate to self so we're subscribed to events
 		centralManager = CBCentralManager.init()
 		centralManager.delegate = self
 	}
 	
 	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+		// Method called right before executing a segue (switching screens)
+		// Using this to pass data to the LightControlViewController
 		if segue.identifier == "showLightControlSegue" {
 			let destination = segue.destination as! LightControlViewController
 			destination.connectedPeripheral = selectedPeripheral
@@ -33,49 +35,71 @@ class DevicesListViewController: UIViewController {
 }
 
 extension DevicesListViewController: CBCentralManagerDelegate {
+	// Method called every time the manager updates its state
 	func centralManagerDidUpdateState(_ central: CBCentralManager) {
+		// Starting to scan for peripherals here, where the manager is ready to do that
 		centralManager.scanForPeripherals(withServices: nil, options: nil)
 	}
 	
-	func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+	// Method called after a peripheral has been discovered
+	func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String: Any], rssi RSSI: NSNumber) {
+		// Only storing a peripheral if it has a name
+		// Putting it into a set so we don't have repeating objects
+		// Reloading the table view after so it will have the latest data in
 		if peripheral.name != nil {
 			peripherals.insert(peripheral)
 			tableView.reloadData()
 		}
 	}
 	
+	// Method called right after we have connected to a peripheral
 	func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+		// Stopping the scanner after we have connected to a peripheral
 		centralManager.stopScan()
+		// Storing the connected peripheral
 		selectedPeripheral = peripheral
+		// Switching to the next screen (because this will be called after a tap on the table view)
 		performSegue(withIdentifier: "showLightControlSegue", sender: self)
 	}
 }
 
 extension DevicesListViewController: UITableViewDataSource {
+	// Method called every time a cell is being drawn on the screen
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		// Creating a cell from our custom class
 		let cell = tableView.dequeueReusableCell(withIdentifier: "discoveredDeviceCell", for: indexPath) as! DiscoveredDeviceTableViewCell
+		// Getting the name that matches the index of the row
+		// A set is a collection of unsorted elements so we are sorting it first (a bit unefficient)
 		let name = peripherals.sorted { (o1, o2) -> Bool in
 			return o1.name! < o2.name!
 		}[indexPath.row].name!
+		// Setting the cell's label to the name
 		cell.nameLabel.text = name
 		return cell
 	}
 	
 	func numberOfSections(in tableView: UITableView) -> Int {
+		// We only need 1 section in this example
 		return 1
 	}
 	
+	// Method to provide the number of rows in the table view
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		// Returning how many elements our set has
 		return peripherals.count
 	}
 }
 
 extension DevicesListViewController: UITableViewDelegate {
+	// Method called after the user taps a cell
 	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		// Deselecting the selected row
 		tableView.deselectRow(at: indexPath, animated: true)
+		// Sorting again as before
 		let peripheral = peripherals.sorted { (o1, o2) -> Bool in
 			return o1.name! < o2.name!
 			}[indexPath.row]
+		// Connecting to the matching peripheral (didConnect will be called when it is done)
 		centralManager.connect(peripheral, options: nil)
 	}
 }
--- BLETest/LightControlViewController.swift
+++ BLETest/LightControlViewController.swift
@@ -10,34 +10,41 @@ import UIKit
 import CoreBluetooth
 
 class LightControlViewController: UIViewController {
-	var connectedPeripheral: CBPeripheral?
-    var writeCharacteristic: CBCharacteristic?
-	private var characteristicUUID = CBUUID(string: "FFE1")
-	private var writeType: CBCharacteristicWriteType = .withoutResponse
+	var connectedPeripheral: CBPeripheral? // The connected peripheral received from the last ViewController
+    var writeCharacteristic: CBCharacteristic? // The characteristic needed to write to the board
+	private var characteristicUUID = CBUUID(string: "FFE1") // The characteristic's address
+	private var writeType: CBCharacteristicWriteType = .withoutResponse // The type of writing (can be with a response as well)
 	
-	@IBOutlet weak var nameLabel: UILabel!
+	@IBOutlet weak var nameLabel: UILabel! // UI instance for the peripheral's name
 	
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+		// Setting the CBPeripheralDelegate to self
 		connectedPeripheral?.delegate = self
+		
+		// Starting to scan the peripheral for the services available and we're not providing any service address
 		connectedPeripheral?.discoverServices(nil)
 		
+		// Setting the label's text with the peripheral's name
 		nameLabel.text = connectedPeripheral?.name
     }
 	
 	@IBAction func lightOnAction(sender: UIButton) {
+		// If the peripheral is nil, just return
 		guard let peripheral = connectedPeripheral else { return }
 		
+		// Sending the "1" String as Data (bytes) to the peripheral
 		if let data = "1".data(using: String.Encoding.utf8) {
 			peripheral.writeValue(data, for: writeCharacteristic!, type: writeType)
 		}
 	}
 	
 	@IBAction func lightOffAction(sender: UIButton) {
+		// If the peripheral is nil, just return
 		guard let peripheral = connectedPeripheral else { return }
 		
+		// Sending the "0" String as Data (bytes) to the peripheral
 		if let data = "0".data(using: String.Encoding.utf8) {
 			peripheral.writeValue(data, for: writeCharacteristic!, type: writeType)
 		}
@@ -45,7 +52,10 @@ class LightControlViewController: UIViewController {
 }
 
 extension LightControlViewController: CBPeripheralDelegate {
+	// Method called when services are being discovered from the peripheral
 	func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+		// Code to discover characteristics of all the services discovered
+		// Using if lets to make sure that the values are not nil
 		if let services = peripheral.services, let peripheral = connectedPeripheral {
 			for service in services {
 				peripheral.discoverCharacteristics([characteristicUUID], for: service)
@@ -53,11 +63,16 @@ extension LightControlViewController: CBPeripheralDelegate {
 		}
 	}
 	
+	// Method called when a characteristic of a service has been discoverred
 	func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+		// For every characteristic we are looking for the write address ("FFE1")
 		for characteristic in service.characteristics! {
 			if characteristic.uuid == characteristicUUID {
+				// Subscribe to this value (so we'll get notified when there is serial data for us..)
 				peripheral.setNotifyValue(true, for: characteristic)
+				// Keep a reference to this characteristic so we can write to it
 				writeCharacteristic = characteristic
+				// Find out writeType
 				writeType = characteristic.properties.contains(.write) ? .withResponse : .withoutResponse
 			}
 		}
